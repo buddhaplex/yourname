64ffb389c542cbceaf2b082a07a58007e04d1631
Eric Andrade <ericandrade@gmail.com>
2015-07-28 11:49:23 +0000

    Practicing setting up scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Yourname::Application.routes.draw do
+  
+root 'names#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
